ee31b77db882d8ef34ea70b50dc8712f27def546
Tom Nguyen <tomn2804@gmail.com>
2021-12-08 21:32:07 -0800

    Init project

---
+++ rekognition/app/src/main/java/edu/uw/minh2804/rekognition/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.uw.minh2804.rekognition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
