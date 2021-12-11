5b7670e4ad1ce098a460b4598e035ae1bd98b636
fretws <fretws@uw.edu>
2021-12-11 01:23:47 -0800

    Implement abstract class GoogleVisionService

--- rekognition/app/src/main/java/edu/uw/minh2804/rekognition/network/GoogleVisionService.kt
+++ rekognition/app/src/main/java/edu/uw/minh2804/rekognition/network/GoogleVisionService.kt
@@ -1,4 +1,41 @@
 package edu.uw.minh2804.rekognition.network
 
-interface GoogleVisionService {
+import android.graphics.Bitmap
+import android.util.Base64
+import java.io.ByteArrayOutputStream
+
+abstract class GoogleVisionService {
+    val BASE_URL = "https://us-central1-rekognition-3d49c.cloudfunctions.net/"
+
+    private fun bitmapToString(bitmap: Bitmap): String {
+        // Scale down bitmap size
+        val scaledBitmap = scaleBitmapDown(bitmap, 640)
+
+        // Convert bitmap to base64 encoded string
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val imageBytes: ByteArray = byteArrayOutputStream.toByteArray()
+        val base64encoded = Base64.encodeToString(imageBytes, Base64.NO_WRAP)
+        return base64encoded
+    }
+
+    private fun scaleBitmapDown(bitmap: Bitmap, maxDimension: Int): Bitmap {
+        val originalWidth = bitmap.width
+        val originalHeight = bitmap.height
+        var resizedWidth = maxDimension
+        var resizedHeight = maxDimension
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension
+            resizedWidth =
+                (resizedHeight * originalWidth.toFloat() / originalHeight.toFloat()).toInt()
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension
+            resizedHeight =
+                (resizedWidth * originalHeight.toFloat() / originalWidth.toFloat()).toInt()
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension
+            resizedWidth = maxDimension
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false)
+    }
 }
