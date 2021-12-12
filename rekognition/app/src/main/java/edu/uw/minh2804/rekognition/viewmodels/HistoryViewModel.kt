46acb2c0302eb57a0d339ce3dc9e07802e55e727
Will Song <wsong6@uw.edu>
2021-12-12 15:13:28 -0800

    Update HistoryFragment to use thumbnail and saved results from ImageProcessingStore

--- rekognition/app/src/main/java/edu/uw/minh2804/rekognition/viewmodels/HistoryViewModel.kt
+++ rekognition/app/src/main/java/edu/uw/minh2804/rekognition/viewmodels/HistoryViewModel.kt
@@ -2,36 +2,37 @@
 
 package edu.uw.minh2804.rekognition.viewmodels
 
+import android.net.Uri
 import android.util.Log
-import androidx.core.net.toUri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import edu.uw.minh2804.rekognition.models.HistoryItem
-import java.io.File
+import edu.uw.minh2804.rekognition.stores.ImageProcessingResult
 
 /**
  * A view model for the HistoryFragment. Holds history for images captured using this app
  * and the associated detected text / objects. It is initialized with the images directory
  * passed from the HistoryFragment.
  */
-class HistoryViewModel(private val imagesDir: File) : ViewModel() {
+class HistoryViewModel : ViewModel() {
 
-    val historyList = initializeHistoryList()
+    private val _historyList = MutableLiveData<List<HistoryItem>>()
+    val historyList : LiveData<List<HistoryItem>>
+        get() = _historyList
 
     init {
-        Log.i(TAG, "HistoryViewModel initialized")
+        Log.d(TAG, "HistoryViewModel initialized")
     }
 
     /**
-     * Initializes the history list from saved data in storage.
+     * Populate the history list from saved data in storage.
      */
-    private fun initializeHistoryList(): List<HistoryItem> {
-        Log.i(TAG, "Initializing the history list")
-        val imageFiles = imagesDir.listFiles()
-        return imageFiles?.map {
-            // TODO: replace placeholder text
-            // TODO: use thumbnail images
-            HistoryItem(image=it.toUri(), text="Hello")
-        } ?: listOf<HistoryItem>()
+    fun populateHistoryList(historyData: Array<ImageProcessingResult>) {
+        Log.d(TAG, "Initializing the history list")
+        _historyList.value = historyData.map {
+            HistoryItem(image=Uri.parse(it.thumbnailUriPath), text=it.result)
+        }
     }
 
     companion object {
