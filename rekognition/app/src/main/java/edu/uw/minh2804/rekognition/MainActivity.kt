b00297e279ef651472433e1bacc6eb726e42eff3
fretws <fretws@uw.edu>
2021-12-12 19:40:04 -0800

    Replace app bar with custom bar

--- rekognition/app/src/main/java/edu/uw/minh2804/rekognition/MainActivity.kt
+++ rekognition/app/src/main/java/edu/uw/minh2804/rekognition/MainActivity.kt
@@ -1,5 +1,18 @@
 package edu.uw.minh2804.rekognition
 
+import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.main_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+}
